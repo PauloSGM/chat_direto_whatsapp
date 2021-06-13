4658353312cf73372c438c85354febb00ef18bd0
PauloSGM <meiotodo@hotmail.com>
2021-06-13 20:31:04 -0300

    reparos

--- script/script.js
+++ script/script.js
@@ -1,6 +1,6 @@
 
 
-		function createWhatsAppLink(){
+		function criaLink(){
 			url = 'https://web.whatsapp.com/send?phone={tel}&text={message}';
 			var tel = "55"+document.getElementById('phone').value;
 			var message = document.getElementById('message').value;
