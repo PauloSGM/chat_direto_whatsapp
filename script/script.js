353d1d2be034ed4d795012a9c36351bfccf56a63
PauloSGM <meiotodo@hotmail.com>
2021-06-13 16:52:19 -0300

    funções

---
+++ script/script.js
@@ -0,0 +1,13 @@
+
+
+		function createWhatsAppLink(){
+			url = 'https://web.whatsapp.com/send?phone={tel}&text={message}';
+			var tel = "55"+document.getElementById('phone').value;
+			var message = document.getElementById('message').value;
+			url = url.replace('{tel}', tel).replace('{message}', message);
+			document.getElementById('envia').href = url;
+		}
+
+
+		
+
